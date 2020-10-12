c234c5bb20188a49bf0837016c743b775c9909ea
hizkiaadrian <hizkia.susanto@st-hughs.ox.ac.uk>
2020-10-12 16:28:55 +0100

    create a tab widget in settings

--- settingsdialog.cpp
+++ settingsdialog.cpp
@@ -1,11 +1,14 @@
 #include "settingsdialog.h"
 #include "ui_settingsdialog.h"
+#include <QVBoxLayout>
 
 SettingsDialog::SettingsDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::SettingsDialog)
 {
     ui->setupUi(this);
+
+    ui->tabWidget->addTab(new QWidget(), "Categories");
 
 }
 
--- settingsdialog.h
+++ settingsdialog.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QDialog>
+#include <QTabWidget>
 
 namespace Ui {
 class SettingsDialog;
